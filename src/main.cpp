6172d23c35314eb6752feba931577573e2d4bc6a
sotoguerrero <danielsg@tuta.io>
2023-11-29 16:45:12 +0100

    OKish, not convinced

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,8 @@
 int main(int argc, char *argv[]) {
     std::string outputPath = "output.wav";
 
-    if (argc < 3) {
-		printf("Invalid arguments\nUse ./binauralplayer <input file> <framerate>\n");
+    if (argc < 2) {
+		printf("Invalid arguments\nUse ./binauralplayer <input file>\n");
 		return 1;
 	}
 
@@ -36,15 +36,8 @@ int main(int argc, char *argv[]) {
 	const int nchannels = twReader.numChannels;
     int totalNumSamples = twReader.numFramesInHeader;
     const int order = sqrt(nchannels)-1;
-	//const int blockSize = BLOCK_SIZE;
-    const int blockSize =sampleRate/atoi(argv[2]);
-/*
-     if (twReader.numChannels != NUM_CHANNELS || twReader.h.SampleRate != SAMPLE_RATE) {
-        std::cout << "Supplied test wav has wrong format - should be [" << NUM_CHANNELS <<"]channels, fs=[" << SAMPLE_RATE<<"]" <<std::endl;
-        return -1;
-    }
-*/
-	
+	const int blockSize = BLOCK_SIZE;
+
     int numReceivers= 2;
 	
     void* hAmbi;
@@ -57,48 +50,58 @@ int main(int argc, char *argv[]) {
     ambi_roomsim_setNumReceivers(hAmbi, numReceivers);      //Sets the number of input SH receivers. 
     ambi_roomsim_setNumSources(hAmbi, nchannels);           //Sets the number of input signals/sources to encode. 
     
-    ambi_roomsim_setSourceX(hAmbi, 0, 3.0f);                //Sets the 'x' coordinate for a specific source index. 
-    ambi_roomsim_setSourceY(hAmbi, 0, 3.0f);
+
+    //const float posX = 3.9f;
+    const float posX = 0.1f;
+    ambi_roomsim_setSourceX(hAmbi, 0, posX);                //Sets the 'x' coordinate for a specific source index. 
+    ambi_roomsim_setSourceY(hAmbi, 0, 3.9f);
     ambi_roomsim_setSourceZ(hAmbi, 0, 1.7f);
-    ambi_roomsim_setSourceX(hAmbi, 1, 3.0f);                //Sets the 'x' coordinate for a specific source index. 
-    ambi_roomsim_setSourceY(hAmbi, 1, 2.5f);
+    ambi_roomsim_setSourceX(hAmbi, 1, posX);                //Sets the 'x' coordinate for a specific source index. 
+    ambi_roomsim_setSourceY(hAmbi, 1, 3.9f);
     ambi_roomsim_setSourceZ(hAmbi, 1, 1.7f);
     
-    ambi_roomsim_setReceiverX(hAmbi, 0, 0.2f);              //Sets the 'x' coordinate for a specific receiver index. 
-    ambi_roomsim_setReceiverY(hAmbi, 0, 0.2f);
+    ambi_roomsim_setReceiverX(hAmbi, 0, 1.8f);              //Sets the 'x' coordinate for a specific receiver index. 
+    ambi_roomsim_setReceiverY(hAmbi, 0, 0.1f);
 	ambi_roomsim_setReceiverZ(hAmbi, 0, 1.7f);
-    ambi_roomsim_setReceiverX(hAmbi, 1, 0.2f);              //Sets the 'x' coordinate for a specific receiver index. 
-    ambi_roomsim_setReceiverY(hAmbi, 1, 0.4f);
+    ambi_roomsim_setReceiverX(hAmbi, 1, 2.2f);              //Sets the 'x' coordinate for a specific receiver index. 
+    ambi_roomsim_setReceiverY(hAmbi, 1, 0.1f);
 	ambi_roomsim_setReceiverZ(hAmbi, 1, 1.7f);
 
     ambi_roomsim_setRoomDimX(hAmbi, 4.0f);
     ambi_roomsim_setRoomDimY(hAmbi, 4.0f);
     ambi_roomsim_setRoomDimZ(hAmbi, 2.5f);
 
-    ambi_roomsim_setWallAbsCoeff(hAmbi, 0, 0, 0.2f);
+    ambi_roomsim_setWallAbsCoeff(hAmbi, 0, 0, 0.8f);
     
     TinyWav twWriter;
     tinywav_open_write(&twWriter, nchannels, sampleRate, TW_FLOAT32, TW_INLINE, outputPath.c_str());
-
     
     int samplesProcessed = 0;
-    
+    int samplesRead;
+
     while (samplesProcessed < totalNumSamples) {
         float buffer[nchannels * blockSize];
-		int samplesRead = tinywav_read_f(&twReader, buffer, blockSize);
+		samplesRead = tinywav_read_f(&twReader, buffer, blockSize);
 		assert(samplesRead > 0 && " Could not read from file!");
 
         float* pFrameData[MAX_NUM_CHANNELS];
-        int frameSize = ambi_roomsim_getFrameSize();
+        int frameSize = ambi_roomsim_getFrameSize(); //128
         
-        pFrameData[0] = &buffer[0];
-        pFrameData[1] = &buffer[blockSize];
-        ambi_roomsim_process(hAmbi, pFrameData, pFrameData, nchannels, numReceivers, frameSize);    
-        
+        if((blockSize % frameSize == 0)){ /* divisible by frame size 512%128==0 */
+            for (int frame = 0; frame < blockSize/frameSize; frame++) { // blockSize/frameSize == 4
+                for (int ch = 0; ch < nchannels; ch++){
+                    pFrameData[ch] = &buffer[ch*blockSize  + frame*frameSize];
+                }
+                /* perform processing */
+                ambi_roomsim_process(hAmbi, pFrameData, pFrameData, nchannels, numReceivers, frameSize);
+            }
+        }
         int samplesWritten = tinywav_write_f(&twWriter, buffer, samplesRead);
         assert(samplesWritten > 0 && "Could not write to file!");
-        samplesProcessed += samplesRead * NUM_CHANNELS;
+        samplesProcessed += samplesRead * nchannels;
     }
+    samplesProcessed -= samplesRead * nchannels;
+    std::cout << samplesProcessed << " / " << totalNumSamples << " samples processed"<<std::endl;
     tinywav_close_read(&twReader);
     tinywav_close_write(&twWriter);
     
